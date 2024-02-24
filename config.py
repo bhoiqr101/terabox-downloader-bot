a377905e12dd15f83a15361d8b1e8e745b61f624
bhoiqr101 <141326163+bhoiqr101@users.noreply.github.com>
2024-02-24 12:57:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID =   # api id
-API_HASH = ""  # api hash
+API_ID = 3304407  # api id
+API_HASH = "c723587400c1122119d9efeabd5e542b"  # api hash
 
-BOT_TOKEN = ""  # bot token
+BOT_TOKEN = "6758254991:AAEfBSpzi1bLV3SuEydus4zEnOk_VdycirM"  # bot token
 
 
 ## REDIS
@@ -9,6 +9,6 @@ HOST = ""  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002139378710  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
-ADMINS = [1317173146]
+ADMINS = [6500072213]
